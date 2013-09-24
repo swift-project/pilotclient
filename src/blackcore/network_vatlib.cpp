01af31d4dab33b3f22ca930944b285eedefb50cb
Mathew Sutcliffe <oktal3700@gmail.com>
2013-09-24 01:04:22 +0100

    fixed bug which was discovered due to a GCC warning

--- src/blackcore/network_vatlib.cpp
+++ src/blackcore/network_vatlib.cpp
@@ -486,9 +486,12 @@ namespace BlackCore
         {
             const char* key = keysValues[0];
             const char* value = keysValues[1];
-            if (key == net->capability_AtcInfo)         { flags |= AcceptsAtisResponses; }
-            else if (key == net->capability_InterimPos) { flags |= SupportsInterimPosUpdates; }
-            else if (key == net->capability_ModelDesc)  { flags |= SupportsModelDescriptions; }
+            if (*value == '1')
+            {
+                if (key == net->capability_AtcInfo)         { flags |= AcceptsAtisResponses; }
+                else if (key == net->capability_InterimPos) { flags |= SupportsInterimPosUpdates; }
+                else if (key == net->capability_ModelDesc)  { flags |= SupportsModelDescriptions; }
+            }
             keysValues += 2;
         }
         emit cbvar_cast(cbvar)->capabilitiesQueryReplyReceived(cbvar_cast(cbvar)->fromFSD(callsign), flags);
