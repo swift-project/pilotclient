dc54621a90897dc495deb245c3f6bb1bbd7c5442
Klaus Basan <klaus.basan@klausbasan.de>
2014-03-05 23:37:06 +0100

    refs #74 , dummy for Doxygen

---
+++ src/blacksim/fscommon/fscommon.h
@@ -0,0 +1,12 @@
+#ifndef FSCOMMON_NAMESPACES_H
+#define FSCOMMON_NAMESPACES_H
+
+// just a dummy header, namespace documentation will go here
+/*! \file */
+
+/*!
+ * \namespace FsCommon
+ * \brief Utility classes for FSX, P3D and FS9.
+ */
+
+#endif // guard
