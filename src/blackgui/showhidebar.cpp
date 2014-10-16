7c5729ff44f99acb71f619c3c26607d9bcdb0f38
Klaus Basan <klaus.basan@klausbasan.de>
2014-10-16 17:08:12 +0200

    refs #335 show / hide bar

---
+++ src/blackgui/showhidebar.cpp
@@ -0,0 +1,31 @@
+/* Copyright (C) 2013
+ * swift project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+#include "showhidebar.h"
+#include "ui_showhidebar.h"
+
+namespace BlackGui
+{
+
+    CShowHideBar::CShowHideBar(QWidget *parent) :
+        QFrame(parent),
+        ui(new Ui::CShowHideBar)
+    {
+        ui->setupUi(this);
+        connect(this->ui->pb_ShowHidePushButton, &QPushButton::clicked, this, &CShowHideBar::toggleShowHide);
+    }
+
+    CShowHideBar::~CShowHideBar()
+    { }
+
+    bool CShowHideBar::isShown() const
+    {
+        return this->ui->pb_ShowHidePushButton->isChecked();
+    }
+} // namespacee
---
+++ src/blackgui/showhidebar.h
@@ -0,0 +1,45 @@
+/* Copyright (C) 2013
+ * swift project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+//! \file
+
+#ifndef BLACKGUI_SHOWHIDEBAR_H
+#define BLACKGUI_SHOWHIDEBAR_H
+
+#include <QFrame>
+
+namespace Ui { class CShowHideBar; }
+
+namespace BlackGui
+{
+    //! Show or hide another section
+    class CShowHideBar : public QFrame
+    {
+        Q_OBJECT
+
+    public:
+        //! Constructor
+        explicit CShowHideBar(QWidget *parent = nullptr);
+
+        //! Destructor
+        ~CShowHideBar();
+
+        //! Is shown?
+        bool isShown() const;
+
+    signals:
+        //! Show or hide cockpit details
+        void toggleShowHide(bool show);
+
+    private:
+        QScopedPointer<Ui::CShowHideBar> ui;
+    };
+
+} // namespace
+#endif // guard
