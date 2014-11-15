2a831f8252992442b0a7adc5d142442444ac9905
Klaus Basan <klaus.basan@klausbasan.de>
2014-11-15 01:33:20 +0100

    refs #288, a server form used in login screen and settings

---
+++ src/blackgui/serverform.cpp
@@ -0,0 +1,82 @@
+/* Copyright (C) 2014
+ * swift project Community / Contributors
+ *
+ * This file is part of swift Project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+#include "serverform.h"
+#include "ui_serverform.h"
+
+using namespace BlackMisc::Network;
+
+namespace BlackGui
+{
+
+    CServerForm::CServerForm(QWidget *parent) :
+        QFrame(parent),
+        ui(new Ui::CNetworkServerForm)
+    {
+        ui->setupUi(this);
+        this->ui->le_Port->setValidator(new QIntValidator(1, 65535, this));
+    }
+
+    CServerForm::~CServerForm()
+    { }
+
+    void CServerForm::setServer(const CServer &server)
+    {
+        CUser user = server.getUser();
+        this->ui->le_NetworkId->setText(user.getId());
+        this->ui->le_RealName->setText(user.getRealName());
+        this->ui->le_Name->setText(server.getName());
+        this->ui->le_Password->setText(user.getPassword());
+        this->ui->le_Description->setText(server.getDescription());
+        this->ui->le_Address->setText(server.getAddress());
+        this->ui->le_Port->setText(QString::number(server.getPort()));
+    }
+
+    BlackMisc::Network::CServer CServerForm::getServer() const
+    {
+        CUser user(
+            this->ui->le_NetworkId->text().trimmed(),
+            this->ui->le_RealName->text().trimmed().simplified(),
+            "",
+            this->ui->le_Password->text().trimmed()
+        );
+        CServer server(
+            this->ui->le_Name->text().trimmed().simplified(),
+            this->ui->le_Description->text().trimmed().simplified(),
+            this->ui->le_Address->text().trimmed(),
+            this->ui->le_Port->text().trimmed().toInt(),
+            user
+        );
+        return server;
+    }
+
+    void CServerForm::setReadOnly(bool readOnly)
+    {
+        this->ui->le_NetworkId->setReadOnly(readOnly);
+        this->ui->le_RealName->setReadOnly(readOnly);
+        this->ui->le_Name->setReadOnly(readOnly);
+        this->ui->le_Description->setReadOnly(readOnly);
+        this->ui->le_Address->setReadOnly(readOnly);
+        this->ui->le_Port->setReadOnly(readOnly);
+        this->ui->le_Password->setReadOnly(readOnly);
+    }
+
+    void CServerForm::showPasswordField(bool show)
+    {
+        this->ui->lbl_Password->setVisible(show);
+        this->ui->le_Password->setVisible(show);
+    }
+
+    BlackMisc::CStatusMessageList CServerForm::validate() const
+    {
+        CServer server = getServer();
+        return server.validate();
+    }
+
+} // namespace
---
+++ src/blackgui/serverform.h
@@ -0,0 +1,57 @@
+/* Copyright (C) 2014
+ * swift project Community / Contributors
+ *
+ * This file is part of swift Project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+//! \file
+
+#ifndef BLACKGUI_NETWORKSERVERFORM_H
+#define BLACKGUI_NETWORKSERVERFORM_H
+
+#include "blackmisc/nwserver.h"
+#include "blackmisc/statusmessagelist.h"
+#include <QFrame>
+#include <QScopedPointer>
+
+namespace Ui { class CNetworkServerForm; }
+
+namespace BlackGui
+{
+
+//! Server form
+    class CServerForm : public QFrame
+    {
+        Q_OBJECT
+
+    public:
+        //! Constructor
+        explicit CServerForm(QWidget *parent = nullptr);
+
+        //! Destructor
+        ~CServerForm();
+
+        //! Set server
+        void setServer(const BlackMisc::Network::CServer &server);
+
+        //! Get server
+        BlackMisc::Network::CServer getServer() const;
+
+        //! Set read only
+        void setReadOnly(bool readOnly);
+
+        //! Show the password field
+        void showPasswordField(bool show);
+
+        //! Validate and provide messages (empty means OK)
+        BlackMisc::CStatusMessageList validate() const;
+
+    private:
+        QScopedPointer<Ui::CNetworkServerForm> ui;
+    };
+}
+
+#endif // guard
