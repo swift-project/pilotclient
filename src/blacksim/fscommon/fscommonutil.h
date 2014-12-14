ca19ee8053d8fee106a1f5fc861d053e8a9adc4f
Klaus Basan <klaus.basan@klausbasan.de>
2014-12-15 00:35:32 +0100

    refs #358, added a utility class for FS

---
+++ src/blacksim/fscommon/fscommonutil.h
@@ -0,0 +1,38 @@
+/* Copyright (C) 2013
+ * swift Project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+//! \file
+
+#ifndef FSCOMMONUTIL_H
+#define FSCOMMONUTIL_H
+
+#include <QString>
+
+namespace BlackSim
+{
+    namespace FsCommon
+    {
+        //! FS9/X utils
+        class CFsCommonUtil
+        {
+        public:
+            CFsCommonUtil();
+
+            //! FSX directory obtained from registry
+            static QString fsxDirFromRegistry();
+
+            //! FSX's simObject directory from registry
+            static QString fsxSimObjectsDirFromRegistry();
+
+        };
+
+    } // namespace
+} // namespace
+
+#endif // guard
