210fb5a53020bb0a8b3b458092d7e367fb5e0a41
Mathew Sutcliffe <oktal3700@gmail.com>
2015-05-09 23:31:18 +0100

    refs #422, #408 Fixed illegal character in dbus interface name which was previously missed.

--- src/plugins/simulator/xplane/xbus_service_proxy.h
+++ src/plugins/simulator/xplane/xbus_service_proxy.h
@@ -13,7 +13,7 @@
 #include <functional>
 
 //! \cond PRIVATE
-#define XBUS_SERVICE_INTERFACENAME "org.swift-project.xbus.service"
+#define XBUS_SERVICE_INTERFACENAME "org.swift_project.xbus.service"
 #define XBUS_SERVICE_OBJECTPATH "/xbus"
 //! \endcond
 
