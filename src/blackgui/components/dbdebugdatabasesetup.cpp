9aebf555bfc5567b20d1a7d10d46ac20061e5bf4
Klaus Basan <klaus.basan@klausbasan.de>
2015-10-30 02:26:53 +0100

    refs #497, added login with settings component and added a small checkbox to enable/disable server debugging

---
+++ src/blackgui/components/dbdebugdatabasesetup.cpp
@@ -0,0 +1,41 @@
+/* Copyright (C) 2015
+ * swift project Community / Contributors
+ *
+ * This file is part of swift Project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+#include "blackmisc/project.h"
+#include "dbdebugdatabasesetup.h"
+#include "ui_dbdebugdatabasesetup.h"
+
+using namespace BlackCore::Data;
+using namespace BlackMisc;
+
+namespace BlackGui
+{
+    namespace Components
+    {
+        CDbDebugDatabaseSetup::CDbDebugDatabaseSetup(QWidget *parent) :
+            QFrame(parent),
+            ui(new Ui::CDbDebugDatabaseSetup)
+        {
+            ui->setupUi(this);
+            this->setEnabled(CProject::isRunningInDeveloperEnvironment());
+            connect(ui->cb_EnableServerDebugging, &QCheckBox::toggled, this, &CDbDebugDatabaseSetup::ps_debugChanged);
+        }
+
+        CDbDebugDatabaseSetup::~CDbDebugDatabaseSetup()
+        { }
+
+        void CDbDebugDatabaseSetup::ps_debugChanged(bool set)
+        {
+            CGlobalSetup gs(m_setup.get());
+            gs.setServerDebugFlag(set);
+            m_setup.set(gs);
+        }
+
+    } // ns
+} // ns
---
+++ src/blackgui/components/dbdebugdatabasesetup.h
@@ -0,0 +1,51 @@
+/* Copyright (C) 2015
+ * swift project Community / Contributors
+ *
+ * This file is part of swift Project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+//! \file
+
+#ifndef BLACKGUI_COMPONENTS_DBDEBUGDATABASESETUP_H
+#define BLACKGUI_COMPONENTS_DBDEBUGDATABASESETUP_H
+
+#include "blackcore/data/globalsetup.h"
+#include <QFrame>
+#include <QScopedPointer>
+
+namespace Ui { class CDbDebugDatabaseSetup; }
+
+namespace BlackGui
+{
+    namespace Components
+    {
+        /*!
+         * Debug settings for DB (only to be used as developer)
+         * \remarks Disabled when not runnig in dev.environment
+         */
+        class CDbDebugDatabaseSetup : public QFrame
+        {
+            Q_OBJECT
+
+        public:
+            //! Constructor
+            explicit CDbDebugDatabaseSetup(QWidget *parent = nullptr);
+
+            //! Dstructor
+            ~CDbDebugDatabaseSetup();
+
+        private slots:
+            //! Changed the debug checkbox
+            void ps_debugChanged(bool set);
+
+        private:
+            QScopedPointer<Ui::CDbDebugDatabaseSetup> ui;
+            BlackCore::CData<BlackCore::Data::GlobalSetup> m_setup {this};   //!< data cache
+        };
+    } // ns
+} // ns
+
+#endif // guard
