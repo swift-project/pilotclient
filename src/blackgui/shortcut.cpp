3d68bfbbba73ca15ac030cd9f93e4ab7a783afe8
Klaus Basan <klaus.basan@klausbasan.de>
2015-12-17 20:02:22 +0100

    refs #535, shortcut class where I keep global key sequences and such to avoid overlapping

---
+++ src/blackgui/shortcut.cpp
@@ -0,0 +1,40 @@
+/* Copyright (C) 2015
+ * swift project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ *
+ * Class based on qLed: Copyright (C) 2010 by P. Sereno, http://www.sereno-online.com
+ */
+
+#include "shortcut.h"
+
+namespace BlackGui
+{
+    const QKeySequence &CShortcut::keyStash()
+    {
+        static const QKeySequence k(Qt::ALT + Qt::Key_S);
+        return k;
+    }
+
+    const QKeySequence &CShortcut::keyClearSelection()
+    {
+        static const QKeySequence k(Qt::CTRL + Qt::Key_C);
+        return k;
+    }
+
+    const QKeySequence &CShortcut::keySelectAll()
+    {
+        static const QKeySequence k(Qt::CTRL + Qt::Key_A);
+        return k;
+    }
+
+    const QKeySequence &BlackGui::CShortcut::keyDisplayFilter()
+    {
+        static const QKeySequence k(Qt::CTRL + Qt::Key_F);
+        return k;
+    }
+
+} // ns
---
+++ src/blackgui/shortcut.h
@@ -0,0 +1,43 @@
+/* Copyright (C) 2015
+ * swift project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ *
+ * Class based on qLed: Copyright (C) 2010 by P. Sereno, http://www.sereno-online.com
+ */
+
+//! \file
+
+#ifndef BLACKGUI_SHORTCUT_H
+#define BLACKGUI_SHORTCUT_H
+
+#include <QShortcut>
+
+namespace BlackGui
+{
+    /*!
+     * Shortcut class to avoid overlapping shortcut defintions
+     */
+    class CShortcut
+    {
+    public:
+        //! No constructor
+        CShortcut() = delete;
+
+        //! For stashing models
+        static const QKeySequence &keyStash();
+
+        //! For deselecting all
+        static const QKeySequence &keyClearSelection();
+
+        //! For selecting all
+        static const QKeySequence &keySelectAll();
+
+        //! Display filter
+        static const QKeySequence &keyDisplayFilter();
+    };
+} // ns
+#endif // CSHORTCUT_H
