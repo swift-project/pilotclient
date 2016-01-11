dda82f75d6832fa208bd8a11e4f3556982fbec41
Roland Winklmeier <Roland.M.Winklmeier@gmail.com>
2016-01-11 16:06:12 +0100

    [g2clib] Remove multiple definitions of templates
    Patch from Fedora 23
    http://pkgs.fedoraproject.org/cgit/rpms/g2clib.git/tree/g2clib-templates.patch

--- src/plugins/weatherdata/gfs/g2clib/drstemplates.c
+++ src/plugins/weatherdata/gfs/g2clib/drstemplates.c
@@ -2,6 +2,29 @@
 #include "grib2.h"
 #include "drstemplates.h"
 
+const struct drstemplate templatesdrs[MAXDRSTEMP] = {
+       // 5.0: Grid point data - Simple Packing
+   { 0, 5, 0, {4,-2,-2,1,1} },
+       // 5.2: Grid point data - Complex Packing
+   { 2, 16, 0, {4,-2,-2,1,1,1,1,4,4,4,1,1,4,1,4,1} },
+       // 5.3: Grid point data - Complex Packing and spatial differencing
+   { 3, 18, 0, {4,-2,-2,1,1,1,1,4,4,4,1,1,4,1,4,1,1,1} },
+       // 5.50: Spectral Data - Simple Packing
+   { 50, 5, 0, {4,-2,-2,1,4} },
+       // 5.51: Spherical Harmonics data - Complex packing
+   { 51, 10, 0, {4,-2,-2,1,-4,2,2,2,4,1} },
+//           // 5.1: Matrix values at gridpoint - Simple packing
+//         { 1, 15, 1, {4,-2,-2,1,1,1,4,2,2,1,1,1,1,1,1} },
+       // 5.40: Grid point data - JPEG2000 encoding
+   { 40, 7, 0, {4,-2,-2,1,1,1,1} },
+       // 5.41: Grid point data - PNG encoding
+   { 41, 5, 0, {4,-2,-2,1,1} },
+       // 5.40000: Grid point data - JPEG2000 encoding
+   { 40000, 7, 0, {4,-2,-2,1,1,1,1} },
+       // 5.40010: Grid point data - PNG encoding
+   { 40010, 5, 0, {4,-2,-2,1,1} }
+} ;
+
 g2int getdrsindex(g2int number)
 /*!$$$  SUBPROGRAM DOCUMENTATION BLOCK
 !                .      .    .                                       .
