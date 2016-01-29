d860d971eb393f25e27f4d1b8e7336c935444c95
Klaus Basan <klaus.basan@klausbasan.de>
2016-01-29 21:32:46 +0100

    refs #587, allow to select model mode (include/exclude)

---
+++ src/blackgui/modelmodeselector.h
@@ -0,0 +1,59 @@
+/* Copyright (C) 2016
+ * swift project Community / Contributors
+ *
+ * This file is part of swift Project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+//! \file
+
+#ifndef BLACKGUI_MODELMODESELECTOR_H
+#define BLACKGUI_MODELMODESELECTOR_H
+
+#include "blackguiexport.h"
+#include "blackmisc/simulation/aircraftmodel.h"
+#include <QFrame>
+#include <QScopedPointer>
+
+namespace Ui { class CModelModeSelector; }
+
+namespace BlackGui
+{
+    /*!
+     * Select the mode
+     */
+    class BLACKGUI_EXPORT CModelModeSelector : public QFrame
+    {
+        Q_OBJECT
+
+    public:
+        //! Constructor
+        explicit CModelModeSelector(QWidget *parent = nullptr);
+
+        //! Destructor
+        ~CModelModeSelector();
+
+        //! Get mode
+        BlackMisc::Simulation::CAircraftModel::ModelMode getMode() const;
+
+        //! Set mode
+        void setValue(BlackMisc::Simulation::CAircraftModel::ModelMode mode);
+
+        //! Set mode
+        void setValue(const BlackMisc::Simulation::CAircraftModel &model);
+
+        //! Read only
+        void setReadOnly(bool readOnly);
+
+    signals:
+        //! Value changed
+        void changed();
+
+    private:
+        QScopedPointer<Ui::CModelModeSelector> ui;
+    };
+}
+
+#endif // guard
