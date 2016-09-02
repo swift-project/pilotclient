1e8090d366813c424cec7ffcfd2536c05d73081a
Klaus Basan <klaus.basan@klausbasan.de>
2016-09-03 00:28:33 +0200

    refs #755, "remove published models" message

--- src/blackgui/components/dbstashcomponent.cpp
+++ src/blackgui/components/dbstashcomponent.cpp
@@ -300,7 +300,7 @@ namespace BlackGui
                 }
                 else
                 {
-                    QString confirm("Remove %1 published models?");
+                    QString confirm("Remove %1 published models from stash?");
                     auto lambda = [this, publishedModels]()
                     {
                         this->unstashModels(publishedModels.getModelStringList(false));
