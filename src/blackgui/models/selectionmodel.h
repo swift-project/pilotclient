988e1b70be024c876368931cbda39e041861eeca
Klaus Basan <klaus.basan@klausbasan.de>
2016-12-17 19:09:40 +0100

    refs #785, interface to access selection model

---
+++ src/blackgui/models/selectionmodel.h
@@ -0,0 +1,34 @@
+/* Copyright (C) 2016
+ * swift project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+//! \file
+
+#ifndef BLACKGUI_SELECTIONMODEL_H
+#define BLACKGUI_SELECTIONMODEL_H
+
+namespace BlackGui
+{
+    namespace Models
+    {
+        //! Allow to get and select objects
+        template <typename ContainerType> class ISelectionModel
+        {
+        public:
+            //! Destructor
+            virtual ~ISelectionModel() {}
+
+            //! Selected objects
+            virtual ContainerType selectedObjects() const = 0;
+
+            //! Select
+            virtual void selectObjects(const ContainerType &selectedObjects) = 0;
+        };
+    } // namespace
+} // namespace
+#endif // guard
