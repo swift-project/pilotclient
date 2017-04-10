45f0cea08363c754371b499d44d1d9f45024f990
Klaus Basan <klaus.basan@klausbasan.de>
2017-04-10 16:41:14 +0200

    refs #933, UI for MCX setup

---
+++ src/blackgui/components/modelconverterxsetupcomponent.cpp
@@ -0,0 +1,56 @@
+/* Copyright (C) 2013
+ * swift project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+#include "modelconverterxsetupcomponent.h"
+#include "ui_modelconverterxsetupcomponent.h"
+#include "blackmisc/statusmessage.h"
+#include "blackmisc/logmessage.h"
+#include <QFileDialog>
+
+using namespace BlackMisc;
+
+namespace BlackGui
+{
+    namespace Components
+    {
+        CModelConverterXSetupComponent::CModelConverterXSetupComponent(QWidget *parent) :
+            QFrame(parent),
+            ui(new Ui::CModelConverterXSetupComponent)
+        {
+            ui->setupUi(this);
+            connect(ui->pb_DirBrowser, &QPushButton::clicked, this, &CModelConverterXSetupComponent::selectBinary);
+            connect(ui->le_McxBinary, &QLineEdit::returnPressed, this, &CModelConverterXSetupComponent::saveSettings);
+            ui->le_McxBinary->setText(m_setting.get());
+        }
+
+        CModelConverterXSetupComponent::~CModelConverterXSetupComponent()
+        { }
+
+        void CModelConverterXSetupComponent::selectBinary()
+        {
+            QString defaultValue = m_setting.get();
+            if (defaultValue.isEmpty())
+            {
+                defaultValue = QDir::currentPath();
+            }
+            const QString fileName = QFileDialog::getOpenFileName(nullptr,
+                                     tr("ModelConverterX binary"), defaultValue,
+                                     "*.exe");
+            ui->le_McxBinary->setText(fileName);
+            this->saveSettings();
+        }
+
+        void CModelConverterXSetupComponent::saveSettings()
+        {
+            const QString t = ui->le_McxBinary->text();
+            const CStatusMessage msg = m_setting.setAndSave(t);
+            CLogMessage::preformatted(msg);
+        }
+    } // ns
+} // ns
---
+++ src/blackgui/components/modelconverterxsetupcomponent.h
@@ -0,0 +1,50 @@
+/* Copyright (C) 2017
+ * swift project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+//! \file
+
+#ifndef BLACKGUI_COMPONENTS_MODELCONVERTERXSETUPCOMPONENT_H
+#define BLACKGUI_COMPONENTS_MODELCONVERTERXSETUPCOMPONENT_H
+
+#include "blackmisc/simulation/modelconverterx.h"
+#include <QFrame>
+
+namespace Ui { class CModelConverterXSetupComponent; }
+namespace BlackGui
+{
+    namespace Components
+    {
+        /**
+        * Setup to start ModelConverterX
+        */
+        class CModelConverterXSetupComponent : public QFrame
+        {
+            Q_OBJECT
+
+        public:
+            //! Ctor
+            explicit CModelConverterXSetupComponent(QWidget *parent = nullptr);
+
+            //! Dtor
+            virtual ~CModelConverterXSetupComponent();
+
+        private:
+            //! File dialog
+            void selectBinary();
+
+            //! Save the settings
+            void saveSettings();
+
+            QScopedPointer<Ui::CModelConverterXSetupComponent> ui;
+            BlackMisc::CSetting<BlackMisc::Simulation::TModelConverterXBinary> m_setting { this };
+        };
+    } // ns
+} // ns
+
+#endif // guard
