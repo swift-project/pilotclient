d1bf788f0755e937846cd3354ac1f674de08b970
Klaus Basan <klaus.basan@klausbasan.de>
2017-04-25 14:36:13 +0200

    Ref T41, independent dialog for downloading swift

---
+++ src/blackgui/components/downloadandinstalldialog.cpp
@@ -0,0 +1,62 @@
+/* Copyright (C) 2017
+ * swift project Community / Contributors
+ *
+ * This file is part of swift Project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+#include "downloadandinstalldialog.h"
+#include "ui_downloadandinstalldialog.h"
+#include <QPushButton>
+#include <QDesktopServices>
+
+using namespace BlackMisc::Db;
+
+namespace BlackGui
+{
+    class COverlayMessagesFrame;
+
+    namespace Components
+    {
+        CDownloadAndInstallDialog::CDownloadAndInstallDialog(QWidget *parent) :
+            QDialog(parent),
+            ui(new Ui::CDownloadAndInstallDialog)
+        {
+            ui->setupUi(this);
+            ui->bb_DownloadInstallDialog->button(QDialogButtonBox::Ok)->setText("Download and install");
+            this->selectionChanged();
+            connect(ui->comp_DistributionInfo, &CDistributionInfoComponent::selectionChanged, this, &CDownloadAndInstallDialog::selectionChanged);
+        }
+
+        CDownloadAndInstallDialog::~CDownloadAndInstallDialog()
+        { }
+
+        bool CDownloadAndInstallDialog::isNewVersionAvailable() const
+        {
+            const bool newVersion = ui->comp_DistributionInfo->isNewVersionAvailable();
+            return newVersion;
+        }
+
+        int CDownloadAndInstallDialog::exec()
+        {
+            const int r = QDialog::exec();
+            if (r != QDialog::Accepted) return r;
+            if (!ui->comp_DistributionInfo->isNewVersionAvailable()) { return QDialog::Rejected; }
+            const CDistribution distribution = ui->comp_DistributionInfo->getCurrentDistribution();
+            if (!distribution.hasDownloadUrls()) { return QDialog::Rejected; }
+
+            // in future, start download and close application
+            // for now, just open URL
+            QDesktopServices::openUrl(distribution.getDownloadUrls().getRandomUrl());
+            return QDialog::Rejected;
+        }
+
+        void CDownloadAndInstallDialog::selectionChanged()
+        {
+            const bool nv = ui->comp_DistributionInfo->isNewVersionAvailable();
+            ui->bb_DownloadInstallDialog->button(QDialogButtonBox::Ok)->setEnabled(nv);
+        }
+    } // ns
+} // ns
---
+++ src/blackgui/components/downloadandinstalldialog.h
@@ -0,0 +1,52 @@
+/* Copyright (C) 2017
+ * swift project Community / Contributors
+ *
+ * This file is part of swift Project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+//! \file
+
+#ifndef BLACKGUI_COMPONENTS_DOWNLOADANDINSTALLDIALOG_H
+#define BLACKGUI_COMPONENTS_DOWNLOADANDINSTALLDIALOG_H
+
+#include "blackgui/blackguiexport.h"
+#include <QDialog>
+
+namespace Ui { class CDownloadAndInstallDialog; }
+namespace BlackGui
+{
+    namespace Components
+    {
+        /**
+         * Download and install swift
+         */
+        class BLACKGUI_EXPORT CDownloadAndInstallDialog : public QDialog
+        {
+            Q_OBJECT
+
+        public:
+            //! Constructor
+            explicit CDownloadAndInstallDialog(QWidget *parent = nullptr);
+
+            //! Destructor
+            virtual ~CDownloadAndInstallDialog();
+
+            //! A new version existing?
+            bool isNewVersionAvailable() const;
+
+            //! \copydoc QDialog::exec
+            virtual int exec() override;
+
+        private:
+            QScopedPointer<Ui::CDownloadAndInstallDialog> ui;
+
+            //! Selection in distribution component changed
+            void selectionChanged();
+        };
+    } // ns
+} // ns
+
+#endif // guard
