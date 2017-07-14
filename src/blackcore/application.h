b3186d6b9110100caffabdb1b023a155270618db
Klaus Basan <klaus.basan@klausbasan.de>
2017-07-14 03:34:32 +0200

    Doxygen

--- src/blackcore/application.h
+++ src/blackcore/application.h
@@ -242,6 +242,7 @@ namespace BlackCore
 
         //! Process all events for some time
         //! \remark unlike QCoreApplication::processEvents this will spend at least the given time in the function, using QThread::msleep
+        //! \sa BlackMisc::CEventLoop
         static void processEventsFor(int milliseconds);
 
         //! Clear the caches
