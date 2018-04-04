6fec158534ce871e1f0f7506612a51255ad8da27
Roland Winklmeier <Roland.M.Winklmeier@gmail.com>
2018-04-04 10:22:24 +0200

    [xswiftbus] Move CAircraftModel into its own file

--- src/xswiftbus/service.h
+++ src/xswiftbus/service.h
@@ -20,6 +20,7 @@
 #include "datarefs.h"
 #include "messages.h"
 #include "navdatareference.h"
+#include "aircraftmodel.h"
 #include <XPLM/XPLMNavigation.h>
 #include <string>
 
@@ -30,73 +31,6 @@
 
 namespace XSwiftBus
 {
-
-    //! Simplified implementation of \sa BlackMisc::Simulation::CDistributor
-    class CDistributor
-    {
-    public:
-        //! Default constructor
-        CDistributor() = default;
-
-        //! Constructor
-        CDistributor(const std::string &description) : m_description(description) {}
-
-        //! \copydoc BlackMisc::Simulation::CDistributor::hasDescription
-        bool hasDescription() const { return !m_description.empty(); }
-
-        //! \copydoc BlackMisc::Simulation::CDistributor::getDescription
-        std::string getDescription() const { return m_description; }
-
-    private:
-        std::string m_description;
-    };
-
-    //! Simplified implementation of \sa BlackMisc::Simulation::CAircraftModel
-    class CAircraftModel
-    {
-    public:
-        CAircraftModel() = default;
-
-        //! \copydoc BlackMisc::Simulation::CAircraftModel::hasDescription
-        bool hasDescription() const { return !m_description.empty(); }
-
-        //! \copydoc BlackMisc::Simulation::CAircraftModel::hasAircraftDesignator
-        bool hasAircraftDesignator() const { return !m_icao.empty(); }
-
-        //! \copydoc BlackMisc::Simulation::CAircraftModel::getName
-        std::string getName() const { return m_name; }
-
-        //! \copydoc BlackMisc::Simulation::CAircraftModel::getDistributor
-        CDistributor getDistributor() const { return m_distributor; }
-
-        //! \copydoc BlackMisc::Simulation::CAircraftModel::getAircraftIcaoCodeDesignator
-        std::string getAircraftIcaoCodeDesignator() const { return m_icao; }
-
-        //! \copydoc BlackMisc::Simulation::CAircraftModel::getModelString
-        std::string getModelString() const { return m_modelString; }
-
-        //! \copydoc BlackMisc::Simulation::CAircraftModel::setAircraftIcaoCode
-        void setAircraftIcaoCode(const std::string &icao) { m_icao = icao; }
-
-        //! \copydoc BlackMisc::Simulation::CAircraftModel::setDescription
-        void setDescription(const std::string &description) { m_description = description; }
-
-        //! \copydoc BlackMisc::Simulation::CAircraftModel::setName
-        void setName(const std::string &name) { m_name = name; }
-
-        //! \copydoc BlackMisc::Simulation::CAircraftModel::setDistributor
-        void setDistributor(const CDistributor &distributor) { m_distributor = distributor; }
-
-        //! \copydoc BlackMisc::Simulation::CAircraftModel::setModelString
-        void setModelString(const std::string &modelString) { m_modelString = modelString; }
-
-    private:
-        std::string m_name;
-        std::string m_icao;
-        std::string m_description;
-        CDistributor m_distributor;
-        std::string m_modelString;
-    };
 
     /*!
      * XSwiftBus service object which is accessible through DBus
