5c5cab3d803213db201e8c670365bda89c447576
Klaus Basan <klaus.basan@klausbasan.de>
2018-08-06 01:41:38 +0200

    Ref T298,settings matching component which can save setup to context

---
+++ src/blackgui/components/settingsmatchingcomponent.cpp
@@ -0,0 +1,58 @@
+/* Copyright (C) 2018
+ * swift project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+#include "settingsmatchingcomponent.h"
+#include "ui_settingsmatchingcomponent.h"
+
+#include "blackgui/guiapplication.h"
+#include "blackcore/context/contextsimulator.h"
+#include "blackmisc/simulation/aircraftmatchersetup.h"
+
+using namespace BlackMisc::Simulation;
+using namespace BlackCore::Context;
+
+namespace BlackGui
+{
+    namespace Components
+    {
+        CSettingsMatchingComponent::CSettingsMatchingComponent(QWidget *parent) :
+            QFrame(parent),
+            ui(new Ui::CSettingsMatchingComponent)
+        {
+            ui->setupUi(this);
+            connect(ui->pb_Save, &QPushButton::released, this, &CSettingsMatchingComponent::onSavePressed);
+            connect(ui->pb_Reload, &QPushButton::released, this, &CSettingsMatchingComponent::onReloadPressed);
+        }
+
+        CSettingsMatchingComponent::~CSettingsMatchingComponent()
+        { }
+
+        void CSettingsMatchingComponent::onSavePressed() const
+        {
+            IContextSimulator *simContext = simulatorContext();
+            if (!simContext) { return; }
+            const CAircraftMatcherSetup setup = ui->editor_MatchingForm->value();
+            simContext->setMatchingSetup(setup);
+        }
+
+        void CSettingsMatchingComponent::onReloadPressed()
+        {
+            IContextSimulator *simContext = simulatorContext();
+            if (!simContext) { return; }
+            const CAircraftMatcherSetup setup = simContext->getMatchingSetup();
+            ui->editor_MatchingForm->setValue(setup);
+        }
+
+        IContextSimulator *CSettingsMatchingComponent::simulatorContext()
+        {
+            if (!sGui || sGui->isShuttingDown() || !sGui->getIContextSimulator()) { return nullptr; }
+            return sGui->getIContextSimulator();
+        }
+    } // ns
+} // ns
---
+++ src/blackgui/components/settingsmatchingcomponent.h
@@ -0,0 +1,51 @@
+/* Copyright (C) 2018
+ * swift project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE file.
+ */
+
+//! \file
+
+#ifndef BLACKGUI_COMPONENTS_SETTINGSMATCHINGCOMPONENT_H
+#define BLACKGUI_COMPONENTS_SETTINGSMATCHINGCOMPONENT_H
+
+#include <QFrame>
+#include <QScopedPointer>
+
+namespace BlackCore { namespace Context { class IContextSimulator; }}
+namespace Ui { class CSettingsMatchingComponent; }
+namespace BlackGui
+{
+    namespace Components
+    {
+        //! Settings for matching component
+        class CSettingsMatchingComponent : public QFrame
+        {
+            Q_OBJECT
+
+        public:
+            //! Constructor
+            explicit CSettingsMatchingComponent(QWidget *parent = nullptr);
+
+            //! Destructor
+            virtual ~CSettingsMatchingComponent() override;
+
+        private:
+            QScopedPointer<Ui::CSettingsMatchingComponent> ui;
+
+            //! Save pressed
+            void onSavePressed() const;
+
+            //! Reload pressed
+            void onReloadPressed();
+
+            //! Network context
+            static BlackCore::Context::IContextSimulator *simulatorContext();
+        };
+    } // ns
+} // ns
+
+#endif // guard
