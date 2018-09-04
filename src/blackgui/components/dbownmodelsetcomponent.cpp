916f2f61808ece69ff86898af0e5ef947a32dbc4
Klaus Basan <klaus.basan@klausbasan.de>
2018-09-04 17:13:18 +0200

    Added missing "order" context menu

--- src/blackgui/components/dbownmodelsetcomponent.cpp
+++ src/blackgui/components/dbownmodelsetcomponent.cpp
@@ -66,7 +66,7 @@ namespace BlackGui
             ui->tvp_OwnModelSet->setCustomMenu(new CLoadModelSetMenu(this));
             ui->tvp_OwnModelSet->setCustomMenu(new CConsolidateWithDbDataMenu(ui->tvp_OwnModelSet, this));
             ui->tvp_OwnModelSet->setCustomMenu(new CConsolidateWithSimulatorModels(ui->tvp_OwnModelSet, this));
-            ui->tvp_OwnModelSet->menuAddItems(CAircraftModelView::MenuLoadAndSave);
+            ui->tvp_OwnModelSet->menuAddItems(CAircraftModelView::MenuLoadAndSave | CAircraftModelView::MenuOrderable);
             ui->tvp_OwnModelSet->setSorting(CAircraftModel::IndexOrderString);
             ui->tvp_OwnModelSet->initAsOrderable();
             ui->tvp_OwnModelSet->setSimulatorForLoading(ui->comp_SimulatorSelector->getValue());
