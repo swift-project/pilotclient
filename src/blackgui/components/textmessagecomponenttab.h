a2e4b42a7f09e52453180b2dd7a5f27f82cd5365
Mat Sutcliffe <oktal3700@gmail.com>
2019-02-22 20:21:44 +0000

    Ref T547 Update license masthead in all source code files.

--- src/blackgui/components/textmessagecomponenttab.h
+++ src/blackgui/components/textmessagecomponenttab.h
@@ -1,10 +1,9 @@
 /* Copyright (C) 2018
  * swift project Community / Contributors
  *
- * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the toplevel
- * directory of this distribution and at http://www.swiftproject.org/license.html. No part of swift project,
- * including this file, may be copied, modified, propagated, or distributed except according to the terms
- * contained in the LICENSE file.
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution. No part of swift project, including this file, may be copied, modified, propagated,
+ * or distributed except according to the terms contained in the LICENSE file.
  */
 
 //! \file
