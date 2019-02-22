e9a4c49d68635050fbb99b633174ec4f1a0472fd
Mat Sutcliffe <oktal3700@gmail.com>
2019-02-22 20:21:44 +0000

    Ref T547 Update license masthead in all source code files.

--- src/blackgui/components/cockpitinfoareacomponent.cpp
+++ src/blackgui/components/cockpitinfoareacomponent.cpp
@@ -2,9 +2,8 @@
  * swift project Community / Contributors
  *
  * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
- * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
- * including this file, may be copied, modified, propagated, or distributed except according to the terms
- * contained in the LICENSE file.
+ * directory of this distribution. No part of swift project, including this file, may be copied, modified, propagated,
+ * or distributed except according to the terms contained in the LICENSE file.
  */
 
 #include "blackgui/components/cockpitinfoareacomponent.h"
