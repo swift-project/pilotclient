27de455f9844b821134b561d0510ac9e2b6c0e40
Lars Toenning <dev@ltoenning.de>
2019-02-23 12:29:57 +0100

    Add/update copyright/license notice

--- src/blackmisc/simulation/flightgear/aircraftmodelloaderflightgear.h
+++ src/blackmisc/simulation/flightgear/aircraftmodelloaderflightgear.h
@@ -1,3 +1,11 @@
+/* Copyright (C) 2019
+ * swift project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution. No part of swift project, including this file, may be copied, modified, propagated,
+ * or distributed except according to the terms contained in the LICENSE file.
+ */
+
 #include "blackmisc/simulation/aircraftmodelloader.h"
 #include <QPointer>
 
--- src/plugins/simulator/flightgearconfig/simulatorflightgearconfigwindow.cpp
+++ src/plugins/simulator/flightgearconfig/simulatorflightgearconfigwindow.cpp
@@ -1,10 +1,9 @@
-/* Copyright (C) 2015
+/* Copyright (C) 2019
  * swift project Community / Contributors
  *
  * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
- * directory of this distribution and at http://www.swift-project.org/license.html. No part of swift project,
- * including this file, may be copied, modified, propagated, or distributed except according to the terms
- * contained in the LICENSE file.
+ * directory of this distribution. No part of swift project, including this file, may be copied, modified, propagated,
+ * or distributed except according to the terms contained in the LICENSE file.
  */
 
 #include "simulatorflightgearconfigwindow.h"
