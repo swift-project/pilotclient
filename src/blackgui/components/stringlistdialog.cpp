76e34d05ab5e217ecd230fa9fc46148d39771bf1
Klaus Basan <klaus.basan@klausbasan.de>
2019-03-20 16:04:17 +0100

    Ref T561, string list dialog, select from "a list of strings"

---
+++ src/blackgui/components/stringlistdialog.cpp
@@ -0,0 +1,41 @@
+/* Copyright (C) 2019
+ * swift project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution. No part of swift project, including this file, may be copied, modified, propagated,
+ * or distributed except according to the terms contained in the LICENSE file.
+ */
+
+#include "stringlistdialog.h"
+#include "ui_stringlistdialog.h"
+
+namespace BlackGui
+{
+    namespace Components
+    {
+        CStringListDialog::CStringListDialog(QWidget *parent) :
+            QDialog(parent),
+            ui(new Ui::CStringListDialog)
+        {
+            ui->setupUi(this);
+            this->setWindowFlags(this->windowFlags() & ~Qt::WindowContextHelpButtonHint);
+            ui->lw_StringList->setSelectionMode(QAbstractItemView::SingleSelection);
+        }
+
+        CStringListDialog::~CStringListDialog()
+        { }
+
+        void CStringListDialog::setStrings(const QStringList &strings)
+        {
+            ui->lw_StringList->clear();
+            ui->lw_StringList->addItems(strings);
+        }
+
+        QString CStringListDialog::getSelectedValue() const
+        {
+            const QList<QListWidgetItem *> selectedItems = ui->lw_StringList->selectedItems();
+            if (selectedItems.isEmpty()) { return {}; }
+            return selectedItems.front()->text();
+        }
+    } // ns
+} // ns
---
+++ src/blackgui/components/stringlistdialog.h
@@ -0,0 +1,48 @@
+/* Copyright (C) 2019
+ * swift project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution. No part of swift project, including this file, may be copied, modified, propagated,
+ * or distributed except according to the terms contained in the LICENSE file.
+ */
+
+//! \file
+
+#ifndef BLACKGUI_COMPONENTS_STRINGLISTDIALOG_H
+#define BLACKGUI_COMPONENTS_STRINGLISTDIALOG_H
+
+#include <QDialog>
+#include <QScopedPointer>
+
+namespace Ui { class CStringListDialog; }
+namespace BlackGui
+{
+    namespace Components
+    {
+        /**
+         * Select from a list of string
+         */
+        class CStringListDialog : public QDialog
+        {
+            Q_OBJECT
+
+        public:
+            //! Ctor
+            explicit CStringListDialog(QWidget *parent = nullptr);
+
+            //! Destructor
+            virtual ~CStringListDialog() override;
+
+            //! Strings
+            void setStrings(const QStringList &strings);
+
+            //! Selected value
+            QString getSelectedValue() const;
+
+        private:
+            QScopedPointer<Ui::CStringListDialog> ui;
+        };
+    } // ns
+} // ns
+
+#endif // guard
