470a0b27f5272070f3509dddc97fec5776ff1f2c
Mat Sutcliffe <oktal3700@gmail.com>
2019-05-07 15:11:40 +0100

    Fix for undefined behaviour in CCollection::erase.

--- src/blackmisc/collection.h
+++ src/blackmisc/collection.h
@@ -207,14 +207,17 @@ namespace BlackMisc
         //! Remove the element pointed to by the given iterator.
         //! \return An iterator to the position of the next element after the one removed.
         //! \fixme Relying on implementation detail of QMap to reinterpret_cast to the necessary iterator type.
-        iterator erase(iterator pos) { return m_impl.erase(reinterpret_cast<QMapNode<T, T> *&>(pos)); }
+        iterator erase(iterator pos)
+        {
+            return m_impl.erase(const_cast<QMapNode<T, T> *&>(reinterpret_cast<const QMapNode<T, T> *&>(pos)));
+        }
 
         //! Remove the range of elements between two iterators.
         //! \return An iterator to the position of the next element after the one removed.
         //! \fixme Relying on implementation detail of QMap to reinterpret_cast to the necessary iterator type.
         iterator erase(iterator it1, iterator it2)
         {
-            while (it1 != it2) { it1 = m_impl.erase(reinterpret_cast<QMapNode<T, T> *&>(it1)); }
+            while (it1 != it2) { it1 = erase(it1); }
             return it1;
         }
 
