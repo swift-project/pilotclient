f2426aeea14348264729157bfb42b5df303b99a8
Klaus Basan <klaus.basan@klausbasan.de>
2019-06-02 01:31:26 +0200

    Ref T658, renamed to MsReverseLookupFile et.al.

---
+++ resources/share/matchingscript/reverseDemo.js
@@ -0,0 +1,16 @@
+(function() {
+  // inObject
+  // returnObject
+  // modelSet, not available in reverse lookup
+  returnObject.aircraftIcao = "C172";
+  returnObject.modified     = true;
+  returnObject.rerun        = true;
+  returnObject.logMessage   = "Changed to C172";
+	
+  return returnObject;
+  
+  // string return is possible
+  // return "Changed ICAO to C172";
+})
+
+
