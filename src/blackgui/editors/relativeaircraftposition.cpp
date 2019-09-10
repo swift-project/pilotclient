4e0f862ccc35da44f281a94be7c1d76bbad7d7e8
Klaus Basan <klaus.basan@klausbasan.de>
2019-09-10 23:48:08 +0200

    Ref T717, UI for relative positions

---
+++ src/blackgui/editors/relativeaircraftposition.cpp
@@ -0,0 +1,58 @@
+/* Copyright (C) 2019
+ * swift project Community / Contributors
+ *
+ * This file is part of swift Project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution. No part of swift project, including this file, may be copied, modified, propagated,
+ * or distributed except according to the terms contained in the LICENSE file.
+ */
+
+#include "relativeaircraftposition.h"
+#include "ui_relativeaircraftposition.h"
+
+using namespace BlackMisc::Geo;
+using namespace BlackMisc::PhysicalQuantities;
+
+namespace BlackGui
+{
+    namespace Editors
+    {
+        CRelativeAircraftPosition::CRelativeAircraftPosition(QWidget *parent) :
+            CForm(parent),
+            ui(new Ui::CRelativeAircraftPosition)
+        {
+            ui->setupUi(this);
+        }
+
+        CRelativeAircraftPosition::~CRelativeAircraftPosition()
+        { }
+
+        void CRelativeAircraftPosition::setReadOnly(bool readOnly)
+        {
+            ui->sb_Distance->setReadOnly(readOnly);
+            ui->sb_RelativeBearing->setReadOnly(readOnly);
+        }
+
+        void CRelativeAircraftPosition::setDistance(const CLength &distance)
+        {
+            const int distMeters = distance.valueInteger(CLengthUnit::m());
+            ui->sb_Distance->setValue(distMeters);
+        }
+
+        CCoordinateGeodetic CRelativeAircraftPosition::getRelativeCoordinate(const CAngle &bearingOffset) const
+        {
+            if (m_originCoordinate.isNull()) { return CCoordinateGeodetic::null(); }
+            const CLength distance(ui->sb_Distance->value(), CLengthUnit::m());
+            CAngle relBearing(ui->sb_RelativeBearing->value(), CAngleUnit::deg());
+            if (!bearingOffset.isNull()) { relBearing += bearingOffset; }
+            return m_originCoordinate.calculatePosition(distance, relBearing);
+        }
+
+        void CRelativeAircraftPosition::displayInfo(const CCoordinateGeodetic &relPos)
+        {
+            const CCoordinateGeodetic p = relPos.isNull() ? this->getRelativeCoordinate() : relPos;
+            ui->le_Info->setText(
+                QStringLiteral("%1 / %2").arg(m_originCoordinate.toQString(true), p.toQString(true))
+            );
+        }
+    } // ns
+} // ns
---
+++ src/blackgui/editors/relativeaircraftposition.h
@@ -0,0 +1,59 @@
+/* Copyright (C) 2019
+ * swift project Community / Contributors
+ *
+ * This file is part of swift Project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution. No part of swift project, including this file, may be copied, modified, propagated,
+ * or distributed except according to the terms contained in the LICENSE file.
+ */
+
+//! \file
+
+#ifndef BLACKGUI_EDITORS_RELATIVEAIRCRAFTPOSITION_H
+#define BLACKGUI_EDITORS_RELATIVEAIRCRAFTPOSITION_H
+
+#include <QFrame>
+#include <QScopedPointer>
+
+#include "blackmisc/geo/coordinategeodetic.h"
+#include "blackgui/editors/form.h"
+
+namespace Ui { class CRelativeAircraftPosition; }
+namespace BlackGui
+{
+    namespace Editors
+    {
+        //! Position relative to other aircraft
+        class CRelativeAircraftPosition : public CForm
+        {
+            Q_OBJECT
+
+        public:
+            //! Constructor
+            explicit CRelativeAircraftPosition(QWidget *parent = nullptr);
+
+            //! Destructor
+            virtual ~CRelativeAircraftPosition() override;
+
+            //! \copydoc CForm::setReadOnly
+            virtual void setReadOnly(bool readOnly) override;
+
+            //! Set origin coordinate
+            void setOriginCoordinate(const BlackMisc::Geo::CCoordinateGeodetic &originCoordinate) { m_originCoordinate = originCoordinate; }
+
+            //! Set the distance
+            void setDistance(const BlackMisc::PhysicalQuantities::CLength &distance);
+
+            //! Relative coordinate based on entered data
+            BlackMisc::Geo::CCoordinateGeodetic getRelativeCoordinate(const BlackMisc::PhysicalQuantities::CAngle &bearingOffset = BlackMisc::PhysicalQuantities::CAngle::null()) const;
+
+            //! Display coordinate info
+            void displayInfo(const BlackMisc::Geo::CCoordinateGeodetic &relPos = BlackMisc::Geo::CCoordinateGeodetic::null());
+
+        private:
+            QScopedPointer<Ui::CRelativeAircraftPosition> ui;
+            BlackMisc::Geo::CCoordinateGeodetic m_originCoordinate;
+        };
+    } // ns
+} // ns
+
+#endif // guard
