be9e2a37f3302da05a478c7e02a9f2f7cc5ea050
Klaus Basan <klaus.basan@klausbasan.de>
2019-11-08 19:55:13 +0100

    [Radar] Rotate speed indicator with own heading

--- src/blackgui/components/radarcomponent.cpp
+++ src/blackgui/components/radarcomponent.cpp
@@ -192,7 +192,6 @@ namespace BlackGui
                             QPen pen(Qt::green, 1);
                             pen.setCosmetic(true);
                             QGraphicsLineItem *li = new QGraphicsLineItem(QLineF({ 0.0, 0.0 }, polarPoint(20.0, headingRad)), dot);
-                            li->setFlags(QGraphicsItem::ItemIgnoresTransformations);
                             li->setPen(pen);
                         }
                     }
