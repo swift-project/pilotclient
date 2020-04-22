46869217ee2a6632a0591867c5ee3e7154645ce8
Klaus Basan <klaus.basan@klausbasan.de>
2020-04-22 16:15:06 +0200

    [FSD] Also log/display socker error string

--- src/blackcore/fsd/fsdclient.h
+++ src/blackcore/fsd/fsdclient.h
@@ -333,7 +333,8 @@ namespace BlackCore
 
             void readDataFromSocket() { this->readDataFromSocketMaxLines(); }
             void readDataFromSocketMaxLines(int maxLines = -1);
-            QString socketErrorToQString(QAbstractSocket::SocketError error);
+            QString socketErrorString(QAbstractSocket::SocketError error) const;
+            static QString socketErrorToQString(QAbstractSocket::SocketError error);
             void parseMessage(const QString &lineRaw);
 
             //! Init. the message types
