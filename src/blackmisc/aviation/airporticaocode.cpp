4a6d94b6d4928ebb9a586635fa9486a0211ab737
Mat Sutcliffe <oktal3700@gmail.com>
2020-08-26 14:27:28 +0100

    Issue #77 Missing copyright header

--- src/blackmisc/aviation/airporticaocode.cpp
+++ src/blackmisc/aviation/airporticaocode.cpp
@@ -1,3 +1,13 @@
+/* Copyright (C) 2013
+ * swift project Community / Contributors
+ *
+ * This file is part of swift project. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution. No part of swift project, including this file, may be copied, modified, propagated,
+ * or distributed except according to the terms contained in the LICENSE file.
+ */
+
+//! \file
+
 #include "blackmisc/aviation/airporticaocode.h"
 #include "blackmisc/compare.h"
 
