23eb283c19415136db8e9767561af6150253da84
Lars Toenning <dev@ltoenning.de>
2021-10-11 19:17:46 +0100

    Issue #96 [Flightgear] Get velocity data from Flightgear in fast update timer timeout

--- src/plugins/simulator/flightgear/fgswiftbusserviceproxy.h
+++ src/plugins/simulator/flightgear/fgswiftbusserviceproxy.h
@@ -120,6 +120,9 @@ namespace BlackSimPlugin::Flightgear
         //! Get own aircraft situation data
         void getOwnAircraftSituationData(FlightgearData *o_flightgearData);
 
+        //! Get own aircraft velocity data
+        void getOwnAircraftVelocityData(FlightgearData *o_flightgearData);
+
         //! Add a text message to the on-screen display
         void addTextMessage(const QString &text);
 
