7cdec8eeb634f49a8d53681337486d42dff6206d
Lars Toenning <dev@ltoenning.de>
2024-10-06 19:09:45 +0200

    build: Remove permissive workaround
    Tested with MSVC 2022, this is no longer required.

--- src/permissiveworkarounds.h
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: Copyright (C) 2018 swift Project Community / Contributors
-// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-swift-pilot-client-1
-
-//! \file
-
-// Workaround for "unexpected token 'identifier'" in combaseapi.h when compiling with /permissive-
-// https://developercommunity.visualstudio.com/solutions/187139/view.html
-struct IUnknown;
