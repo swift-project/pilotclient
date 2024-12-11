20dfb6c07c4706b291efa5a6ccc59d4fc5d73364
Lars Toenning <dev@ltoenning.de>
2024-12-11 17:08:19 +0100

    build: Fetch opus via conan

---
+++ conanfile.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: Copyright (C) swift Project Community / Contributors
+# SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-swift-pilot-client-1
+
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+
+class SwiftRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("opus/1.3.1")
+
+    def layout(self):
+        cmake_layout(self)
