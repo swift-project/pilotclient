41e531eb35dd065f59b507a83c9120f24151c72f
Lars Toenning <dev@ltoenning.de>
2024-12-12 09:08:19 +0100

    build: Fetch libevent via conan

--- conanfile.py
+++ conanfile.py
@@ -11,6 +11,10 @@ class SwiftRecipe(ConanFile):
 
     def requirements(self):
         self.requires("opus/1.3.1")
+        self.requires("libevent/2.1.12")
+
+    def configure(self):
+        self.options["libevent"].with_openssl = False
 
     def layout(self):
         cmake_layout(self)
