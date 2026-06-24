92da3a6658392eb065327bf193f1d60674111ec9
Lars Toenning <dev@ltoenning.de>
2026-06-24 21:19:19 +0200

    fix: Correct wrong assert message

--- src/misc/aviation/aircraftengine.cpp
+++ src/misc/aviation/aircraftengine.cpp
@@ -13,12 +13,12 @@ namespace swift::misc::aviation
 {
     CAircraftEngine::CAircraftEngine(int number, bool on) : m_number(number), m_on(on)
     {
-        Q_ASSERT_X(number > 0, "CAircraftEngine", "Engine numbers have to be > 1");
+        Q_ASSERT_X(number > 0, "CAircraftEngine", "Engine numbers have to be >= 1");
     }
 
     void CAircraftEngine::setNumber(int number)
     {
-        Q_ASSERT_X(number > 0, "setNumber", "Engine numbers have to be > 1");
+        Q_ASSERT_X(number > 0, "setNumber", "Engine numbers have to be >= 1");
         m_number = number;
     }
 
